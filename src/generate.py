634313eafee27ad3871ad5d810d2ae20ffa7f112
Paul Daniel Faria <Nashenas88@users.noreply.github.com>
2023-01-08 14:59:05 -0800

    Implement random search

--- src/generate.py
+++ src/generate.py
@@ -1,4 +1,5 @@
 import pyrosim.pyrosim as pyrosim
+import random
 
 
 def Create_World():
@@ -41,10 +42,10 @@ def Create_Brain():
     pyrosim.Send_Sensor_Neuron(name=2, linkName='FrontLeg')
     pyrosim.Send_Motor_Neuron(name=3, jointName='Torso_BackLeg')
     pyrosim.Send_Motor_Neuron(name=4, jointName='Torso_FrontLeg')
-    pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=3, weight=-1.0)
-    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=-1.0)
-    pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=4, weight=-1.0)
-    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=-1.0)
+    for sensor in [0, 1, 2]:
+        for motor in [3, 4]:
+            pyrosim.Send_Synapse(sourceNeuronName=sensor,
+                                 targetNeuronName=motor, weight=random.random() * 2 - 1)
     pyrosim.End()
 
 
---
+++ src/search.py
@@ -0,0 +1,4 @@
+import os
+
+os.system("python3 src/generate.py")
+os.system("python3 src/simulate.py")
